2dd81d832f8f0b18b92ce18cf3bd37598cd146b0
unknown <anmolvarshney05>
2016-01-02 16:43:46 +0530

    Updated Source

--- src/Browser_Actions.py
+++ src/Browser_Actions.py
@@ -5,8 +5,7 @@ import os
 class subtitle:
     def __init__(self, file_path, md5):
         self.url = "http://api.thesubdb.com/?action=download&hash=" + md5 + "&language=en"
-        # self.header = {'User-Agent': 'SubsDl (Subtitle Downloader; https://github.com/anmolvarshney05)'}
-        self.header = {'User-Agent': 'SubDB/1.0 (Anmol/0.1; https://github.com/anmolvarshney05/SPOJ-Solutions)'}
+        self.header = {'User-Agent': 'SubDB/1.0 (Anmol/1.0; https://github.com/anmolvarshney05/VLC-with-Subs)'}
         self.fname = os.path.splitext(file_path)[0]
         self.req = urllib2.Request(self.url, '', self.header)
 
--- src/Get_md5.py
+++ src/Get_md5.py
@@ -1,17 +1,5 @@
 import hashlib
 import os
-# lambda is another method of defining a function
-# iter (callable function, iterate until you find this character)
-# b"str" interpret b as binary
-# with open takes care of closing file automatically
-
-# My Version to get MD5
-# def md5(file_path):
-#     h = hashlib.md5()
-#     with open(file_path, "rb") as f:
-#         for chunk in iter(lambda: f.read(4096), b""):
-#             h.update(chunk)
-#     return h.hexdigest()
 
 
 def get_hash(name):
